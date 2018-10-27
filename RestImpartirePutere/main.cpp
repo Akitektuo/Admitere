8ca7e5255ffff4ca09a85b6f0a6dfba7baf789f7
Akitektuo <alexcopindean@yahoo.com>
2018-10-27 18:04:37 +0300

    Add mod algorithm

--- RestImpartirePutere/main.cpp
+++ RestImpartirePutere/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+// (a * b) % c = ((a % c) * (b % c)) % c
+int rest(int base, int power, int divisor) {
+    if (power == 0) {
+        return 1;
+    }
+    return ((base % divisor) * (rest(base, power - 1, divisor) % divisor)) % divisor;
+}
+
 int main() {
-    cout << "Hello world!" << endl;
+    int i = 0, j = 0, k = 0;
+    cin >> i >> j >> k;
+    cout << rest(i, j, k);
     return 0;
 }
